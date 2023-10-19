0edce5a3e409859dd67982081190b4be86b6d17b
NotEdwin <notedwin.music@gmail.com>
2023-10-20 02:50:23 +0800

    update sidebar

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -6,34 +6,34 @@ import RenderTag from "./RenderTag";
 const RightSideBar = () => {
   const hotQuestions = [
     {
-      _id: 1,
+      _id: "1",
       title:
         "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
     },
     {
-      _id: 2,
+      _id: "2",
       title: "Async/Await Function Not Handling Errors Properly",
     },
     {
-      _id: 3,
+      _id: "3",
       title: "Redux Toolkit Not Updating State as Expected",
     },
     {
-      _id: 4,
+      _id: "4",
       title: "How do I use express as a custom server in NextJS?",
     },
     {
-      _id: 5,
+      _id: "5",
       title: "How to Perfectly Center a Div with Tailwind CSS?",
     },
   ];
 
   const popularTags = [
-    { _id: 1, name: "javascript", totalQuestions: 5 },
-    { _id: 2, name: "react", totalQuestions: 5 },
-    { _id: 3, name: "nextjs", totalQuestions: 7 },
-    { _id: 4, name: "html", totalQuestions: 2 },
-    { _id: 5, name: "css", totalQuestions: 10 },
+    { _id: "1", name: "javascript", totalQuestions: 5 },
+    { _id: "2", name: "react", totalQuestions: 5 },
+    { _id: "3", name: "nextjs", totalQuestions: 7 },
+    { _id: "4", name: "html", totalQuestions: 2 },
+    { _id: "5", name: "css", totalQuestions: 10 },
   ];
 
   return (
@@ -43,7 +43,7 @@ const RightSideBar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question.id}`}
+              href={`/questions/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
