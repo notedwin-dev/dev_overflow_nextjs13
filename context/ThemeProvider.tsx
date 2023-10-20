24a1513c9e779fa1117b2e4d62a4c3c3713c12bb
NotEdwin <notedwin.music@gmail.com>
2023-10-20 22:58:47 +0800

    add a console log to update vercel

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -30,6 +30,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
     handleThemeChange();
   }, [mode]);
 
+  console.log("");
+
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
