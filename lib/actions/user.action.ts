95cf2014305d093f6d7a1e0f47ff4496549b2057
NotEdwin <notedwin.music@gmail.com>
2023-10-20 23:04:21 +0800

    fix eslint err

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -83,9 +83,7 @@ export async function deleteUser(userData: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct("_id");
 
     // delete user questions
     await Question.deleteMany({ author: user._id });
