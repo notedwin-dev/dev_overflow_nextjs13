54823014c1f0d07dede49f5a7b58b5c4c1394f21
NotEdwin <notedwin.music@gmail.com>
2023-10-23 04:17:36 +0800

    update page with type of any

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -35,7 +35,7 @@ export default async function Collection() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {results.questions.length > 0 ? (
-          results.questions.map((question) => (
+          results.questions.map((question: any) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
