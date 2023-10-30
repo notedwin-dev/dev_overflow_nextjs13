89886d093f57109df4699eb12a76b2493bcd5b11
NotEdwin <notedwin.music@gmail.com>
2023-10-30 22:25:30 +0800

    removed a div

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -40,41 +40,40 @@ const AllAnswers = async ({
       <div>
         {result?.answers.map((answer) => (
           <article key={answer._id} className="light-border border-b py-10">
-            <div className="flex items-center justify-between">
-              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
-                <Link
-                  href={`/profile/${answer.author.clerkId}`}
-                  className="flex flex-1 items-start gap-1 sm:items-center"
-                >
-                  <Image
-                    src={answer.author.picture}
-                    alt="Author picture"
-                    width={18}
-                    height={18}
-                    className="rounded-full object-cover max-sm:mt-0.5"
-                  />
-                  <div className="flex flex-col sm:flex-row sm:items-center">
-                    <p className="body-semibold text-dark-300_light-700">
-                      {answer.author.name}
-                    </p>
-                    <p className="small-regular text-light-400_light-500 ml-1 mt-0.5 line-clamp-1">
-                      {`answered ${getTimestamp(answer.createdAt)}`}
-                    </p>
-                  </div>
-                </Link>
-                <div className="flex justify-end">
-                  <Votes
-                    type="answer"
-                    itemId={JSON.stringify(answer._id)}
-                    userId={JSON.stringify(userId)}
-                    upvotes={answer.upvotes.length}
-                    hasUpvoted={answer.upvotes.includes(userId)}
-                    downvotes={answer.downvotes.length}
-                    hasDownvoted={answer.downvotes.includes(userId)}
-                  />
+            <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+              <Link
+                href={`/profile/${answer.author.clerkId}`}
+                className="flex flex-1 items-start gap-1 sm:items-center"
+              >
+                <Image
+                  src={answer.author.picture}
+                  alt="Author picture"
+                  width={18}
+                  height={18}
+                  className="rounded-full object-cover max-sm:mt-0.5"
+                />
+                <div className="flex flex-col sm:flex-row sm:items-center">
+                  <p className="body-semibold text-dark-300_light-700">
+                    {answer.author.name}
+                  </p>
+                  <p className="small-regular text-light-400_light-500 ml-1 mt-0.5 line-clamp-1">
+                    {`answered ${getTimestamp(answer.createdAt)}`}
+                  </p>
                 </div>
+              </Link>
+              <div className="flex justify-end">
+                <Votes
+                  type="answer"
+                  itemId={JSON.stringify(answer._id)}
+                  userId={JSON.stringify(userId)}
+                  upvotes={answer.upvotes.length}
+                  hasUpvoted={answer.upvotes.includes(userId)}
+                  downvotes={answer.downvotes.length}
+                  hasDownvoted={answer.downvotes.includes(userId)}
+                />
               </div>
             </div>
+
             <ParseHTML html={answer.content} />
           </article>
         ))}
